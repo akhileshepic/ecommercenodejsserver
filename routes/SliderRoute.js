07a552ab7ee3499c1bebf01c6f6c6860eeadd9af
Ajay Chauhan <ajay.digitalflavers@gmail.com>
2025-01-10 17:37:30 +0530

    dfdsf

--- routes/SliderRoute.js
+++ routes/SliderRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { multipleImage } from "../helpers/UploadFile.js";
 import { CreatFun, getAll, imageDelete } from "../controllers/Category/Category.js";
+import authenticateJWT from "../middleware/Auth.js";
 
 const Router = express.Router();
 
-Router.post('/', multipleImage('HomeSlider'), CreatFun);
-Router.get('/', getAll)
-Router.delete('/:id', imageDelete)
+Router.post('/', multipleImage('HomeSlider'), authenticateJWT, CreatFun);
+Router.get('/', authenticateJWT, getAll)
+Router.delete('/:id', authenticateJWT, imageDelete)
 
 export default Router
