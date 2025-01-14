35985018722ee9636d3656f226e6edada8d5438b
Ajay Chauhan <ajay.digitalflavers@gmail.com>
2025-01-14 11:52:24 +0530

    dsfdsf

--- controllers/User/UserController.js
+++ controllers/User/UserController.js
@@ -173,20 +173,32 @@ const Login = async (req, res) => {
     }
 };
 
-const Logout = async (req, res) => {
+const Logout = async (request, response) => {
     try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(400).json({ message: 'Token is required' });
+        const userid = request.user //middleware
+        console.log(userid)
+        const cookiesOption = {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None"
         }
-        res.status(200).json({ message: 'Logout successful' });
 
+        response.clearCookie("accessToken", cookiesOption)
+        response.clearCookie("refreshToken", cookiesOption)
+
+
+
+        return response.json({
+            message: "Logout successfully",
+            error: false,
+            success: true
+        })
     } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "Server error",
-            error: error.message,
-        });
+        return response.status(500).json({
+            message: error.message || error,
+            error: true,
+            success: false
+        })
     }
 }
 
